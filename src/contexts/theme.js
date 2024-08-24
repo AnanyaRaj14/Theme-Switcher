ede2494d6ad52f31b36548ab3e07ba928a617f50
Ananya Raj <ananyam748@gmail.com>
2024-08-24 17:09:05 +0530

    msg

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -1,10 +1,15 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const ThemeContext = createContext({
     themeMode: "light",
     darkTheme: () => {},
     lightTheme: () => {},
 })
+
+// const [night, setNight] = useState(false);
+//     export const toggleNight = () => {
+//         setNight(!night);
+//     }
 
 export const ThemeProvider = ThemeContext.Provider
 
