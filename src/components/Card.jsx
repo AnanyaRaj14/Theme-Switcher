8cb535d4dda2cdbb8a10b27e9342bbb8a17cc9c4
Ananya Raj <ananyam748@gmail.com>
2024-08-24 22:38:27 +0530

    images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,7 @@ export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src={themeMode === "dark" ? "https://plus.unsplash.com/premium_photo-1685959222430-9ab84326847f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : "https://plus.unsplash.com/premium_photo-1663013006667-e07512fd39d2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="Morning" />
+                <img className="p-8 rounded-t-lg" src={themeMode === "dark" ? "https://m.media-amazon.com/images/I/51lo9aJ1MoL._AC_UF1000,1000_QL80_.jpg" : "https://cdn.pixabay.com/photo/2015/04/27/22/25/sunset-742744_640.jpg"} alt="Morning" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
@@ -17,9 +17,8 @@ export default function Card() {
                         {themeMode === "dark" ? "When life gets too overwhelming, just look up at the night sky and loose yourself for a while." : "For each new morning let there be flow of love. Let there be light of happiness in every direction"}
                     </h5>
                 </a>
-                <div className="flex items-center justify-between">
+                <div className="flex flex-col items-center justify-between font-bold mt-5 my-2">
                     <a
-                        href="/"
                         className="mx-20 mt-8 bg-gray-400 text-black  hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg
                          hover:bg-gray-600 text-sm px-5 py-2.5 text-center  dark:text-black dark:bg-white dark:hover:bg-gray-600"
 
@@ -31,8 +30,8 @@ export default function Card() {
                     <span
                         className={`text-black ${visible ? "" : "hidden"} dark:text-white`}
                     >
-                        {themeMode === "dark" ? "dark" : "Light"}
-                        {/* console.log("false") */}
+                        {themeMode === "dark" ? " Welcome to the Dark Mode..." : "Welcome to the Light Mode..."}
+                       
                     </span>
                 </div>
             </div>
